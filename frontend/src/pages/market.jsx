0bf555d18372813d060d483c880e7b2415a353cc
ljhmd00 <lmsmd00@naver.com>
2023-06-07 16:36:21 +0900

    feat: market 페이지

--- frontend/src/pages/market.jsx
+++ frontend/src/pages/market.jsx
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
 function Market() {
     return <div></div>;
 }
 export default Market;
-=======
-function market() {
-    return <div>market</div>;
-}
-
-export default market;
->>>>>>> develop
