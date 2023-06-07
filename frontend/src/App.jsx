91dbcca2d8cabf1782bf702239befd44fedb3ac1
ljhmd00 <lmsmd00@naver.com>
2023-06-07 16:19:08 +0900

    test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import NotFound from "./pages/notFound";
 import Main from "./pages/main";
+
 function App() {
     return (
         <div>
+            hello
             <Routes>
                 <Route path="/" element={<Main />} />
                 <Route path="*" elememt={<NotFound />} />
