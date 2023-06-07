a0be362af98642a18ac4462c894c27c05aca802e
ljhmd00 <lmsmd00@naver.com>
2023-06-07 19:41:16 +0900

    route 추가

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import NotFound from "./pages/notFound";
 import Main from "./pages/main";
+import Market from "./pages/market";
 
 function App() {
     return (
-        <div>
-            <Routes>
-                <Route path="/" element={<Main />} />
-                <Route path="*" elememt={<NotFound />} />
-            </Routes>
-        </div>
+        <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="*" elememt={<NotFound />} />
+            <Route path="/market" elememt={<Market />} />
+        </Routes>
     );
 }
 
