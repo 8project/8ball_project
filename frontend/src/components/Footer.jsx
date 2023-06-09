7152c21dfb14e2593fcbd5210b36632782494510
ljhmd00 <lmsmd00@naver.com>
2023-06-10 03:21:06 +0900

    feat: Footer 메뉴 추가

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,18 +1,34 @@
-import React from "react";
+import { Box, Text } from "@chakra-ui/react";
+import { BsShop, BsShopWindow } from "react-icons/bs";
+import { FaVoteYea, FaUserCircle, FaHome } from "react-icons/fa";
 
 const Footer = () => {
-  return (
-    <div className="fixed bottom-0">
-      <div
-        className="bg-black text-white flex justify-between items-center "
-        style={{ width: "800px" }}
-      >
-        <img src="/img/8ball.jpg" alt="8ball" className="w-10 h-10" />
-        <div>8Ball Project</div>
-        <div>wallet</div>
-      </div>
-    </div>
-  );
+    return (
+        <Box className="fixed bottom-0 bg-black  text-white max-w-[460px] min-w-[460px] font-mono">
+            <Box className="flex justify-between gap-10 mt-2 mb-2">
+                <Box className="ml-4 flex flex-col items-center hover:text-gray-300">
+                    <BsShop className="text-3xl " />
+                    <Text className="text-sm">Market</Text>
+                </Box>
+                <Box className="flex flex-col items-center hover:text-gray-300">
+                    <BsShopWindow className="text-3xl" />
+                    <Text className="text-sm"> Owner Market</Text>
+                </Box>
+                <Box className="flex flex-col items-center hover:text-gray-300">
+                    <FaHome className="text-3xl" />
+                    <Text className="font-semibold text-sm">Main</Text>
+                </Box>
+                <Box className="flex flex-col items-center hover:text-gray-300">
+                    <FaVoteYea className="text-3xl" />
+                    <Text className="text-sm">Vote</Text>
+                </Box>
+                <Box className="mr-4 flex flex-col items-center hover:text-gray-300">
+                    <FaUserCircle className="text-3xl" />
+                    <Text className="text-sm">MyPage</Text>
+                </Box>
+            </Box>
+        </Box>
+    );
 };
 
 export default Footer;
