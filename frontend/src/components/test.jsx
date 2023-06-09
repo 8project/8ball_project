e4257edfc0ed170ed0eede54b93714d1c6d586b1
ljhmd00 <lmsmd00@naver.com>
2023-06-10 03:20:35 +0900

    test: test file

---
+++ frontend/src/components/test.jsx
@@ -0,0 +1,50 @@
+import { Box, Image } from "@chakra-ui/react";
+import nft1 from "../images/testFoloder/20220501000342_0-001.jpg";
+import nft2 from "../images/testFoloder/20220501000342_0-002.jpg";
+import nft3 from "../images/testFoloder/20220501000342_0-003.jpg";
+import nft4 from "../images/testFoloder/20220501000342_0-004.jpg";
+import nft5 from "../images/testFoloder/20220501000342_0-005.jpg";
+import nft6 from "../images/testFoloder/20220501000342_0-006.jpg";
+import nft7 from "../images/testFoloder/20220501000342_0-007.jpg";
+import nft8 from "../images/testFoloder/20220501000342_0-008.jpg";
+import nft9 from "../images/testFoloder/20220501000342_0-009.jpg";
+import nft10 from "../images/testFoloder/20220501000342_0-010.jpg";
+import nft11 from "../images/testFoloder/20220501000342_0-011.jpg";
+import nft12 from "../images/testFoloder/20220501000342_0-012.jpg";
+import nft13 from "../images/testFoloder/20220501000342_0-013.jpg";
+import nft14 from "../images/testFoloder/20220501000342_0-014.jpg";
+import nft15 from "../images/testFoloder/20220501000342_0-015.jpg";
+import nft16 from "../images/testFoloder/20220501000342_0-016.jpg";
+import nft17 from "../images/testFoloder/20220501000342_0-017.jpg";
+import nft18 from "../images/testFoloder/20220501000342_0-018.jpg";
+import nft19 from "../images/testFoloder/20220501000342_0-019.jpg";
+import nft20 from "../images/testFoloder/20220501000342_0-020.jpg";
+
+const Test = () => {
+    return (
+        <Box className=" grid grid-cols-5 gap-1 mt-20">
+            <Image boxSize="50px" src={nft1} />
+            <Image boxSize="50px" src={nft2} />
+            <Image boxSize="50px" src={nft3} />
+            <Image boxSize="50px" src={nft4} />
+            <Image boxSize="50px" src={nft5} />
+            <Image boxSize="50px" src={nft6} />
+            <Image boxSize="50px" src={nft7} />
+            <Image boxSize="50px" src={nft8} />
+            <Image boxSize="50px" src={nft9} />
+            <Image boxSize="50px" src={nft10} />
+            <Image boxSize="50px" src={nft11} />
+            <Image boxSize="50px" src={nft12} />
+            <Image boxSize="50px" src={nft13} />
+            <Image boxSize="50px" src={nft14} />
+            <Image boxSize="50px" src={nft15} />
+            <Image boxSize="50px" src={nft16} />
+            <Image boxSize="50px" src={nft17} />
+            <Image boxSize="50px" src={nft18} />
+            <Image boxSize="50px" src={nft19} />
+            <Image boxSize="50px" src={nft20} />
+        </Box>
+    );
+};
+
+export default Test;
