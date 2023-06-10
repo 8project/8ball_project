5199df15fea64b03fa7ea354223d6afb8c8c458e
ljhmd00 <lmsmd00@naver.com>
2023-06-10 14:41:02 +0900

    desgin: main 디자인 수정

--- frontend/src/pages/main.jsx
+++ frontend/src/pages/main.jsx
@@ -8,7 +8,6 @@ function Main() {
         <Box className="flex justify-center">
             <Box className="hidden lg:inline-block mr-20 mt-40">
                 <Image src={logo} boxSize={300} />
-
                 <Text className="-mt-14 ml-6 font-mono font-extrabold">
                     진짜 조각 투자, 오직 8ball에서!
                 </Text>
@@ -16,12 +15,13 @@ function Main() {
                     8ball에서는 NFT를 조각 발행해 지분을 발행합니다.
                 </Text>
             </Box>
-            <Box className="max-w-[460px] bg-gray-50">
+            <Box className="min-w-[460px] max-w-[460px] border-l-2 border-r-2">
                 <Box className="flex flex-col h-screen justify-center items-center ">
                     <Header />
-                    <Test />
-                    <Box className="flex-1 flex justify-center items-center">
-                        <Box className="text-center"></Box>
+                    <Box className="flex-1 flex justify-center">
+                        <Box className="text-center">
+                            <Test />
+                        </Box>
                     </Box>
                     <Footer />
                 </Box>
