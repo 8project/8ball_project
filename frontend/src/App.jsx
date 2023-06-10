3335eddee4b067ccccba68511a7fd2c40397e58d
ljhmd00 <lmsmd00@naver.com>
2023-06-10 14:40:02 +0900

    feat: route 추가

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,19 @@ import { Routes, Route } from "react-router-dom";
 import NotFound from "./pages/notFound";
 import Main from "./pages/main";
 import Market from "./pages/market";
+import OwnerMarket from "./pages/ownerMarket";
+import Vote from "./pages/vote";
+import MyPage from "./pages/mypage";
 
 function App() {
     return (
         <Routes>
             <Route path="/" element={<Main />} />
-            <Route path="*" elememt={<NotFound />} />
-            <Route path="/market" elememt={<Market />} />
+            <Route path="*" element={<NotFound />} />
+            <Route path="/market" element={<Market />} />
+            <Route path="/ownermarket" element={<OwnerMarket />} />
+            <Route path="/vote" element={<Vote />} />
+            <Route path="/mypage" element={<MyPage />} />
         </Routes>
     );
 }
