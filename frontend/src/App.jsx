a33dea0fe356a30afc6900ecce46db784b70387f
zzeongho <junghoo022@gmail.com>
2023-06-12 20:11:26 +0900

    props추가

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,18 +5,23 @@ import Market from "./pages/market";
 import OwnerMarket from "./pages/ownerMarket";
 import Vote from "./pages/vote";
 import MyPage from "./pages/mypage";
+import { useState } from "react";
 
 function App() {
-    return (
-        <Routes>
-            <Route path="/" element={<Main />} />
-            <Route path="*" element={<NotFound />} />
-            <Route path="/market" element={<Market />} />
-            <Route path="/ownermarket" element={<OwnerMarket />} />
-            <Route path="/vote" element={<Vote />} />
-            <Route path="/mypage" element={<MyPage />} />
-        </Routes>
-    );
+  const [account, setAccount] = useState("");
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={<Main account={account} setAccount={setAccount} />}
+      />
+      <Route path="*" element={<NotFound />} />
+      <Route path="/market" element={<Market />} />
+      <Route path="/ownermarket" element={<OwnerMarket />} />
+      <Route path="/vote" element={<Vote />} />
+      <Route path="/mypage" element={<MyPage />} />
+    </Routes>
+  );
 }
 
 export default App;
