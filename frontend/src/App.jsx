85080eef95a82b581e429d75a37c513c8b2ee2f6
zzeongho <junghoo022@gmail.com>
2023-06-14 22:28:57 +0900

    mypage에 props내려줌

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,26 +12,26 @@ import SideInfo from "./components/SideInfo";
 import Footer from "./components/Footer";
 
 function App() {
-    const [account, setAccount] = useState("");
-    return (
-        <Box className="flex justify-center bg-gray-200 min-h-screen">
-            <SideInfo />
-            <Box className="relative">
-                <Header account={account} setAccount={setAccount} />
-                <Box className="flex justify-center lg:min-w-[800px] lg:max-w-[800px] min-w-[460px] max-w-[460px]  min-h-screen bg-white">
-                    <Routes>
-                        <Route path="/" element={<Main />} />
-                        <Route path="*" element={<NotFound />} />
-                        <Route path="/market" element={<Market />} />
-                        <Route path="/pieceMarket" element={<PieceMarket />} />
-                        <Route path="/vote" element={<Vote />} />
-                        <Route path="/mypage" element={<MyPage />} />
-                    </Routes>
-                </Box>
-                <Footer />
-            </Box>
+  const [account, setAccount] = useState("");
+  return (
+    <Box className="flex justify-center bg-gray-200 min-h-screen">
+      <SideInfo />
+      <Box className="relative">
+        <Header account={account} setAccount={setAccount} />
+        <Box className="flex justify-center lg:min-w-[800px] lg:max-w-[800px] min-w-[460px] max-w-[460px]  min-h-screen bg-white">
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="*" element={<NotFound />} />
+            <Route path="/market" element={<Market />} />
+            <Route path="/pieceMarket" element={<PieceMarket />} />
+            <Route path="/vote" element={<Vote />} />
+            <Route path="/mypage" element={<MyPage account={account} />} />
+          </Routes>
         </Box>
-    );
+        <Footer />
+      </Box>
+    </Box>
+  );
 }
 
 export default App;
