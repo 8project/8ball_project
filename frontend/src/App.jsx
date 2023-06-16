e37c54cfac4135674be3a3946a901b6adac36cab
ljhmd00 <lmsmd00@naver.com>
2023-06-16 19:46:33 +0900

    desgin: app, sideInfo ui 수정

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,31 +17,33 @@ import onSale from "./pages/onSale";
 import Login from "./pages/login";
 
 function App() {
-  const [account, setAccount] = useState("");
-  return (
-    <Box className="flex justify-center bg-gray-200 min-h-screen">
-      <SideInfo />
-      <Box className="relative">
-        <Header account={account} setAccount={setAccount} />
-        <Box className="flex justify-center lg:min-w-[800px] lg:max-w-[800px] min-w-[460px] max-w-[460px]  min-h-screen bg-white">
-          <Routes>
-            <Route path="/" element={<Main />} />
-            <Route path="*" element={<NotFound />} />
-            <Route path="/market" element={<Market />} />
-            <Route path="/pieceMarket" element={<PieceMarket />} />
-            <Route path="/vote" element={<Vote />} />
-            <Route path="/login" element={<Login account={account} />} />
-            <Route path="/mypage" element={<MyPage account={account} />} />
-            <Route path="/mypage/myNft" element={<myNft />} />
-            <Route path="/mypage/myPieceNft" element={<myPieceNft />} />
-            <Route path="/mypage/inProgress" element={<inProgress />} />
-            <Route path="/mypage/onSale" element={<onSale />} />
-          </Routes>
+    const [account, setAccount] = useState("");
+    return (
+        <Box className="flex justify-center bg-gray-200 min-h-screen">
+            <Box className="relative xl:w-[500px]">
+                <SideInfo />
+            </Box>
+            <Box className="relative">
+                <Header account={account} setAccount={setAccount} />
+                <Box className="flex justify-center lg:min-w-[800px] lg:max-w-[800px] min-w-[460px] max-w-[460px]  min-h-screen bg-white">
+                    <Routes>
+                        <Route path="/" element={<Main />} />
+                        <Route path="*" element={<NotFound />} />
+                        <Route path="/market" element={<Market />} />
+                        <Route path="/pieceMarket" element={<PieceMarket />} />
+                        <Route path="/vote" element={<Vote />} />
+                        <Route path="/login" element={<Login account={account} />} />
+                        <Route path="/mypage" element={<MyPage account={account} />} />
+                        <Route path="/mypage/myNft" element={<myNft />} />
+                        <Route path="/mypage/myPieceNft" element={<myPieceNft />} />
+                        <Route path="/mypage/inProgress" element={<inProgress />} />
+                        <Route path="/mypage/onSale" element={<onSale />} />
+                    </Routes>
+                </Box>
+                <Footer />
+            </Box>
         </Box>
-        <Footer />
-      </Box>
-    </Box>
-  );
+    );
 }
 
 export default App;
