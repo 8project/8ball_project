0d557a9331a30c38858964916507c65e6afd0d10
zzeongho <junghoo022@gmail.com>
2023-06-16 13:51:12 +0900

    ""

--- frontend/src/pages/main.jsx
+++ frontend/src/pages/main.jsx
@@ -2,10 +2,10 @@ import { Box } from "@chakra-ui/react";
 import Test from "../components/test";
 
 function Main() {
-    return (
-        <Box className="mt-[82px] mb-[72px] lg:max-w-[800px] max-w-[460px]">
-            <Test />
-        </Box>
-    );
+  return (
+    <Box className="mt-[82px] mb-[72px] lg:max-w-[800px] max-w-[460px]">
+      <Test />
+    </Box>
+  );
 }
 export default Main;
