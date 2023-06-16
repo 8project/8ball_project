4dccf8e42dbc7740a9f88e2ee049122f54740e9d
zzeongho <junghoo022@gmail.com>
2023-06-16 13:42:13 +0900

    라우트 추가

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,11 @@ import MyPage from "./pages/mypage";
 import Header from "./components/Header";
 import SideInfo from "./components/SideInfo";
 import Footer from "./components/Footer";
+import myNft from "./pages/myNft";
+import myPieceNft from "./pages/myPieceNft";
+import inProgress from "./pages/inProgress";
+import onSale from "./pages/onSale";
+import Login from "./pages/login";
 
 function App() {
   const [account, setAccount] = useState("");
@@ -25,7 +30,12 @@ function App() {
             <Route path="/market" element={<Market />} />
             <Route path="/pieceMarket" element={<PieceMarket />} />
             <Route path="/vote" element={<Vote />} />
+            <Route path="/login" element={<Login account={account} />} />
             <Route path="/mypage" element={<MyPage account={account} />} />
+            <Route path="/mypage/myNft" element={<myNft />} />
+            <Route path="/mypage/myPieceNft" element={<myPieceNft />} />
+            <Route path="/mypage/inProgress" element={<inProgress />} />
+            <Route path="/mypage/onSale" element={<onSale />} />
           </Routes>
         </Box>
         <Footer />
