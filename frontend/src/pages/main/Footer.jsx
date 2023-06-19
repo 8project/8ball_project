7b734da4692f16186b284188c2880ea51f5d20bd
ljhmd00 <lmsmd00@naver.com>
2023-06-20 00:50:43 +0900

    fix: main페이지 수정

--- frontend/src/pages/main/Footer.jsx
+++ frontend/src/pages/main/Footer.jsx
@@ -1,32 +1,32 @@
 import { Box } from "@chakra-ui/react";
 import {
-  AiOutlineInstagram,
-  AiOutlineTwitter,
-  AiFillFacebook,
-  AiOutlineYoutube,
+    AiOutlineInstagram,
+    AiOutlineTwitter,
+    AiFillFacebook,
+    AiOutlineYoutube,
 } from "react-icons/ai";
 
 const Footer = () => {
-  return (
-    <Box className="bg-black  text-gray-400 font-semibold flex  justify-between items-center  ">
-      <Box className="mb-6 ml-10">
-        <Box className="mt-4 sm:text-lg text-md">Team 8ball</Box>
-        <Box className="mt-2 sm:text-md text-xs">Contract: 조성윤, 송우석</Box>
-        <Box className="mt-2 sm:text-md text-xs">Frontend: 이정호, 이주혁</Box>
-        <Box className="mt-2 sm:text-md text-xs">TECHIT BCS3 8Team</Box>
-      </Box>
-      <Box className="bm-6 mr-10 flex gap-2">
-        <AiOutlineInstagram className="sm:text-4xl" />
-        <AiOutlineTwitter className="sm:text-4xl" />
-        <AiFillFacebook className="sm:text-4xl" />
-        <AiOutlineYoutube className="sm:text-4xl" />
-      </Box>
-      <Box className="bm-6 mr-10">
-        <Box className="mt-2 sm:text-md text-xs">이용약관</Box>
-        <Box className="mt-2 sm:text-md text-xs">개인정보처리방침</Box>
-        <Box className="mt-2 sm:text-md text-xs">자주묻는질문</Box>
-      </Box>
-    </Box>
-  );
+    return (
+        <Box className="mt-40 bg-black  text-gray-400 font-semibold flex  justify-between items-center  ">
+            <Box className="mb-6 ml-10">
+                <Box className="mt-4 sm:text-lg text-md">Team 8ball</Box>
+                <Box className="mt-2 sm:text-md text-xs">Contract: 조성윤, 송우석</Box>
+                <Box className="mt-2 sm:text-md text-xs">Frontend: 이정호, 이주혁</Box>
+                <Box className="mt-2 sm:text-md text-xs">TECHIT BCS3 8Team</Box>
+            </Box>
+            <Box className="bm-6 mr-10 flex gap-2">
+                <AiOutlineInstagram className="sm:text-4xl" />
+                <AiOutlineTwitter className="sm:text-4xl" />
+                <AiFillFacebook className="sm:text-4xl" />
+                <AiOutlineYoutube className="sm:text-4xl" />
+            </Box>
+            <Box className="bm-6 mr-10">
+                <Box className="mt-2 sm:text-md text-xs">이용약관</Box>
+                <Box className="mt-2 sm:text-md text-xs">개인정보처리방침</Box>
+                <Box className="mt-2 sm:text-md text-xs">자주묻는질문</Box>
+            </Box>
+        </Box>
+    );
 };
 export default Footer;
--- frontend/src/pages/main/main.jsx
+++ frontend/src/pages/main/main.jsx
@@ -1,15 +1,20 @@
 import { Box } from "@chakra-ui/react";
 import MainSlider from "./MainSlider";
+import Test from "../../components/test";
 import Faq from "./Faq";
 import Footer from "./Footer";
 
 function Main() {
-  return (
-    <Box className="mt-[82px] mb-[72px] lg:max-w-[800px] max-w-[460px]">
-      <MainSlider />
-      <Faq />
-      <Footer />
-    </Box>
-  );
+    return (
+        <Box className="mt-[82px] mb-[72px] lg:max-w-[800px] max-w-[460px]">
+            <MainSlider />
+            <Box className="flex flex-col items-center justify-center mt-20 mb-10">
+                <Test />
+                <Box>테스트 중입니다</Box>
+            </Box>
+            <Faq />
+            <Footer />
+        </Box>
+    );
 }
 export default Main;
