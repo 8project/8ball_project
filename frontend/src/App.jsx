992c50b0c9ebb73028880aa9e03dffdf015c1176
ljhmd00 <lmsmd00@naver.com>
2023-06-21 15:45:44 +0900

    feat: pieceMarket 작업(pieceNft 추가)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import { Box } from "@chakra-ui/react";
 import NotFound from "./pages/notFound";
 import Main from "./pages/main/main";
 import Market from "./pages/market/market";
-import PieceMarket from "./pages/pieceMarket";
+import PieceMarket from "./pages/pieceMarket/pieceMarket";
 import Vote from "./pages/vote";
 import MyPage from "./pages/mypage";
 import Header from "./components/Header";
@@ -17,33 +17,33 @@ import onSale from "./pages/onSale";
 import Login from "./pages/login";
 
 function App() {
-  const [account, setAccount] = useState("");
-  return (
-    <Box className="flex justify-center bg-gray-200 min-h-screen">
-      <Box className="relative xl:w-[500px]">
-        <SideInfo />
-      </Box>
-      <Box className="relative">
-        <Header account={account} setAccount={setAccount} />
-        <Box className="flex justify-center lg:min-w-[800px] lg:max-w-[800px] min-w-[460px] max-w-[460px]  min-h-screen bg-white">
-          <Routes>
-            <Route path="/" element={<Main />} />
-            <Route path="*" element={<NotFound />} />
-            <Route path="/market" element={<Market />} />
-            <Route path="/pieceMarket" element={<PieceMarket />} />
-            <Route path="/vote" element={<Vote />} />
-            <Route path="/login" element={<Login account={account} />} />
-            <Route path="/mypage" element={<MyPage account={account} />} />
-            <Route path="/mypage/myNft" element={<myNft />} />
-            <Route path="/mypage/myPieceNft" element={<myPieceNft />} />
-            <Route path="/mypage/inProgress" element={<inProgress />} />
-            <Route path="/mypage/onSale" element={<onSale />} />
-          </Routes>
+    const [account, setAccount] = useState("");
+    return (
+        <Box className="flex justify-center bg-gray-200 min-h-screen">
+            <Box className="relative xl:w-[500px]">
+                <SideInfo />
+            </Box>
+            <Box className="relative">
+                <Header account={account} setAccount={setAccount} />
+                <Box className="flex justify-center lg:min-w-[800px] lg:max-w-[800px] min-w-[460px] max-w-[460px]  min-h-screen bg-white">
+                    <Routes>
+                        <Route path="/" element={<Main />} />
+                        <Route path="*" element={<NotFound />} />
+                        <Route path="/market" element={<Market />} />
+                        <Route path="/pieceMarket" element={<PieceMarket />} />
+                        <Route path="/vote" element={<Vote />} />
+                        <Route path="/login" element={<Login account={account} />} />
+                        <Route path="/mypage" element={<MyPage account={account} />} />
+                        <Route path="/mypage/myNft" element={<myNft />} />
+                        <Route path="/mypage/myPieceNft" element={<myPieceNft />} />
+                        <Route path="/mypage/inProgress" element={<inProgress />} />
+                        <Route path="/mypage/onSale" element={<onSale />} />
+                    </Routes>
+                </Box>
+                <BottomMenu />
+            </Box>
         </Box>
-        <BottomMenu />
-      </Box>
-    </Box>
-  );
+    );
 }
 
 export default App;
--- frontend/src/pages/pieceMarket.jsx
+++
@@ -1,6 +0,0 @@
-import { Box } from "@chakra-ui/react";
-
-function PieceMarket() {
-    return <Box className="mt-[82px] mb-[72px] lg:max-w-[800px] max-w-[460px]">pieceMarket</Box>;
-}
-export default PieceMarket;
