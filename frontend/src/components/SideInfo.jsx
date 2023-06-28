9c67c884086fb781f276088c41a4d7a2a627e954
ljhmd00 <lmsmd00@naver.com>
2023-06-28 17:55:27 +0900

    add Tenada font 추가

--- frontend/src/components/SideInfo.jsx
+++ frontend/src/components/SideInfo.jsx
@@ -5,10 +5,10 @@ function SideInfo() {
     return (
         <Box className="fixed center hidden xl:inline-block ml-10 mr-20 mt-40">
             <Image src={logo} boxSize={300} />
-            <Text className="-mt-14 ml-6 font-mono font-extrabold">
+            <Text className="-mt-14 ml-10  font-extrabold font-['Tenada'] text-lg">
                 진짜 조각 투자, 오직 8ball에서!
             </Text>
-            <Text className="mt-2 ml-6 font-mono font-semibold text-sm">
+            <Text className="mt-2 ml-8 font-['Tenada'] text-md">
                 8ball에서는 조각낸 NFT로 지분을 발행합니다.
             </Text>
         </Box>
