7821b0c2d6b194a568282314b48fcea3245c0355
zzeongho <junghoo022@gmail.com>
2023-06-29 18:55:31 +0900

    myPage 업데이트

--- frontend/src/pages/myPage/myNft.jsx
+++ frontend/src/pages/myPage/myNft.jsx
@@ -1,22 +1,19 @@
-import { Box, Text } from "@chakra-ui/react";
-import React from "react";
+import { Box, Text, Image } from "@chakra-ui/react";
+import testImg from "../../images/testFoloder/20220501000342_0.jpg";
 
 const myNft = () => {
   return (
     <Box className="mt-[82px] mb-[72px] lg:max-w-[800px] max-w-[460px]">
-      <Text>votePage</Text>
-      <div className="border-2 ml-12 mt-5 mb-12 flex flex-col items-center rounded-lg w-48 md:w-72 shadow-2xl">
-        <img
-          src="img/pieceNft.png"
-          alt=""
-          className="w-72 h-80 rounded-lg object-cover"
-          crossOrigin="anonymous"
-        />
-        <div className="text-white w-full p-2 bg-gradient-to-t from-[#454545] to-transparent rounded-lg pt-5 -mt-20">
-          <strong className="text-xl">lalala</strong>
-          <p className="display-inline">babababa</p>
-        </div>
-      </div>
+      <Text>My NFT</Text>
+      <Box className="flex flex-col justify-center items-center border rounded-md mb-10 ">
+        <Image src={testImg} className="w-[256px] rounded-t-md " />
+        <Box className="bg-gray-100 w-full px-4 py-1">
+          <Text>Kongz #7332</Text>
+          <div className="bg-blue-300 justify-center text-center p-4">
+            List for sell
+          </div>
+        </Box>
+      </Box>
     </Box>
   );
 };
--- frontend/src/pages/myPage/onSale.jsx
+++ frontend/src/pages/myPage/onSale.jsx
@@ -1,7 +1,21 @@
-import React from "react";
-import { Box, Text } from "@chakra-ui/react";
-const onSale = () => {
-  return <div>onSale</div>;
+import { Box, Text, Image } from "@chakra-ui/react";
+import testImg from "../../images/testFoloder/20220501000342_0.jpg";
+
+const myNft = () => {
+  return (
+    <Box className="mt-[82px] mb-[72px] lg:max-w-[800px] max-w-[460px]">
+      <Text>On Sale</Text>
+      <Box className="flex flex-col justify-center items-center border rounded-md mb-10 ">
+        <Image src={testImg} className="w-[256px] rounded-t-md " />
+        <Box className="bg-gray-100 w-full px-4 py-1">
+          <Text>Kongz #7332</Text>
+          <div className="bg-blue-300 justify-center text-center p-4">
+            List for sell
+          </div>
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
-export default onSale;
+export default myNft;
