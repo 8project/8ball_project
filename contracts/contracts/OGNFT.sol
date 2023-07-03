931cde0e2aac76dd7491ae89e2ff9f1d13cbb247
Doit-Goal <swspolice@naver.com>
2023-07-03 15:29:17 +0900

    OGNFT 수정

--- contracts/contracts/OGNFT.sol
+++ contracts/contracts/OGNFT.sol
@@ -5,14 +5,22 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract OGNFT is ERC721Enumerable {
+    //https://olbm.mypinata.cloud/ipfs/QmU52T5t4bXtoUqQYStgx39DdXy3gLQq7KDuF1F9g3E9Qy 강사님 피나타 주소로 배포할 것 
     string public URI;
 
-    constructor(string memory _uri) ERC721("TestFor", "LL721_1") {
-        URI = _uri;
-    } 
+    uint maxAMount;
 
-    function mintNFT(uint _tokenID) public {
-        _mint(msg.sender, _tokenID); 
+    constructor(string memory _name, string memory _symbol, string memory _URI, uint _maxAmount) ERC721(_name,_symbol) {
+        URI = _URI;
+        maxAMount = _maxAmount;
+    }
+
+    function mintNFT() public {
+        require(totalSupply() < maxAMount, "no more mint");
+        uint tokenId = totalSupply() + 1;
+        _mint(msg.sender, tokenId);
+
+        // metadataUri[tokenId] = _metadataUri;
     }
 
     function tokenURI(uint _tokenID) public override view returns(string memory) {
