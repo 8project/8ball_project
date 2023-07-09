a19f9966b60281e9db544ae0f0a4b3e231bd4b47
zzeongho <junghoo022@gmail.com>
2023-07-10 01:07:33 +0900

    완료된민팅 Button 비활성화(isDisabled)

--- frontend/src/pages/market/funding/FundingNftCard.jsx
+++ frontend/src/pages/market/funding/FundingNftCard.jsx
@@ -13,24 +13,19 @@ const FundingNftCard = ({ indexId, account }) => {
   const [buyerArray, setBuyerArray] = useState([]);
   const { isOpen, onOpen, onClose } = useDisclosure();
   const isFundingCompleted = buyerArray.length === 20;
-  // const [OGIndex, setOGIndex] = useState();
 
   const getOGTokenURI = async () => {
     try {
       const priceResponse = await MarketContract.methods
         .OGNftList(indexId)
         .call();
-      console.log(priceResponse);
       setPrice(web3.utils.fromWei(Number(priceResponse.price), "ether"));
-      console.log(price);
-      // const tokenId = Number(response.OGTokenId);
 
       const response = await OGNFTContract.methods
         .tokenURI(Number(priceResponse.OGTokenId))
         .call();
       const metadataResponse = await axios.get(response);
       setTokenData(metadataResponse.data);
-      //   console.log(metadataResponse.data);
     } catch (error) {
       console.error(error);
     }
@@ -72,37 +67,32 @@ const FundingNftCard = ({ indexId, account }) => {
           </Box>
           <Box className="bg-gray-100 w-full px-4 py-1">
             <Box className="flex justify-between">
-              <Text>{tokenData.name}</Text>
+              <div>{tokenData.name}</div>
               <Text className="cursor-default text-sm font-semibold text-blue-500 rounded-lg">
                 Inprogress
               </Text>
             </Box>
-            <Box className="text-xs mt-2">
-              <Box>{tokenData.description}</Box>
-              <Box>
-                {tokenData.attributes.map((v, i) => {
-                  return (
-                    <Box key={i}>
-                      {v.trait_type} : {v.value}
-                    </Box>
-                  );
-                })}
-              </Box>
-            </Box>
-            <Box className="flex justify-around border-t mt-2">
-              <Text className="text-blue-400 text-sm mt-1">Each Piece : </Text>
-              <Text className="text-blue-500 font-semibold mt-1">
-                {price / 20} ETH
-              </Text>
-            </Box>
+            <div>{tokenData.description}</div>
+            <div>
+              {tokenData.attributes.map((v, i) => (
+                <div key={i}>
+                  {v.trait_type} {v.value}
+                </div>
+              ))}
+            </div>
+            <Text className="text-blue-400 text-sm mt-1">Each Piece</Text>
+            <Text className="text-blue-500 font-semibold mt-1">
+              {price / 20} ETH
+            </Text>
           </Box>
           <Box className="bg-gray-100 w-full flex justify-center py-2">
             <Button
               colorScheme="blue"
-              className="font-bold text-white  px-4 py-2 rounded-md "
+              className="font-bold text-white px-4 py-2 rounded-md"
               onClick={
                 account ? onOpen : () => alert("Need to Connect Metamask")
               }
+              isDisabled={isFundingCompleted}
             >
               Funding
             </Button>
