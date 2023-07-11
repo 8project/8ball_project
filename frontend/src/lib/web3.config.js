789b6f23c88b68479c19121a5415261f242bff40
Doit-Goal <swspolice@naver.com>
2023-07-11 17:56:48 +0900

    web3 컨피그 수정

--- frontend/src/lib/web3.config.js
+++ frontend/src/lib/web3.config.js
@@ -6,11 +6,11 @@ import Web3 from "web3";
 const web3 = new Web3(window.ethereum);
 
 export const OGNFTContractAddress =
-  "0xd1C94c4Bc1264a822DfEB177B93F783c8cf32382";
+  "0xa031F0366E1995FA6DcEE80A9D341294787a9D80";
 export const MarketContractAddress =
-  "0xC286Eda454dE8669Eb9D7dd93edBDA786b01aCa3";
+  "0xf8Ef21E972483c79dfC5605C8ed8739F1aFBD212";
 export const PieceMarketContractAddress =
-  "0xE281e6e5EaFe2db3C7E90f1230D4A5d6930062fD";
+  "0x0689a897a994b32Ff6227cBb4e3859c7683e9e9c";
 
 export const OGNFTContract = new web3.eth.Contract(
   OGNftABI,
@@ -28,5 +28,5 @@ export const PieceMarketContract = new web3.eth.Contract(
 );
 
 // 닷엔브 메타데이터 : https://olbm.mypinata.cloud/ipfs/QmU52T5t4bXtoUqQYStgx39DdXy3gLQq7KDuF1F9g3E9Qy
-// BAYC 원본 : https://harlequin-melted-loon-875.mypinata.cloud/ipfs/QmWH7coyZWc2e8GuYo5YAzpS7PkU14tn8fXHStvjvSuKVF
-// BAYC 조각 메타데이터 : https://harlequin-melted-loon-875.mypinata.cloud/ipfs/QmUfUwne1AXTmPhrtQPghMzwLf2ZKbhqHGB1iBSdFxDuxr
+// BAYC 원본 : https://harlequin-melted-loon-875.mypinata.cloud/ipfs/QmaHB5QDmYJ3ArGH8rnuGqoxEtKStDJZyZQMWLsxymQu91
+// BAYC 조각 메타데이터 : https://harlequin-melted-loon-875.mypinata.cloud/ipfs/QmNNwfj2zZc7z3XxGWTc9Hhpx85y1PTj2TZpUdG8ohaDAa
