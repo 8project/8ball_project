409985d185e3f95e47b3f2e4432c07435d9e9acc
zzeongho <junghoo022@gmail.com>
2023-07-12 17:32:35 +0900

    voteNftCard 업데이트

--- frontend/src/pages/vote/voteNftCard.jsx
+++ frontend/src/pages/vote/voteNftCard.jsx
@@ -1,88 +1,121 @@
-import VoteNftModal from "./voteNftModal";
+import {
+  Box,
+  Card,
+  Stack,
+  CardBody,
+  Heading,
+  Button,
+  Image,
+  Text,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+} from "@chakra-ui/react";
 
 const VoteNftCard = ({
-    v,
-    i,
-    bestOffer,
-    selectedOption,
-    isSubmitted,
-    handleOptionClick,
-    handleSubmit,
-    handleConfirmation,
-    isConfirmationOpen,
+  v,
+  i,
+  bestOffer,
+  selectedOption,
+  isSubmitted,
+  handleOptionClick,
+  handleSubmit,
+  handleConfirmation,
+  isConfirmationOpen,
 }) => {
-    return (
-        <div>
-            <div>
-                <div
-                    key={i}
-                    className={`lg:max-w-[800px] max-w-[460px] flex justify-center items-center mb-4 ${
-                        isSubmitted ? "bg-gray-400" : "bg-gray-200"
-                    } pb-12 pl-6`}
-                >
-                    <div className="m-2">
-                        <img src={v.data.image} alt="pieceNft" className="w-10 h-10" />
-                    </div>
-
-                    <div className="m-2 p-1 text-center">
-                        <div>Offer price</div>
-                        <div className="text-blue-500">{bestOffer} ETH</div>
-                    </div>
-
-                    <div className="m-2 p-1 text-center">
-                        <div className="">Duration</div>
-                        <div>0618~0619</div> // {/*기간도 설정해줘야함 */}
-                    </div>
-
-                    <div className="mt-6 ml-4">
-                        <div>Vote</div>
-                        <div>
-                            <div className="flex items-center">
-                                <button
-                                    className={`option-button ${
-                                        selectedOption === "up" ? "selected" : ""
-                                    }`}
-                                    onClick={() => handleOptionClick("up")}
-                                    disabled={isSubmitted}
-                                ></button>
-                                <div className="ml-2">Up</div>
-                            </div>
-                            <div className="flex items-center">
-                                <button
-                                    className={`option-button ${
-                                        selectedOption === "down" ? "selected" : ""
-                                    }`}
-                                    onClick={() => handleOptionClick("down")}
-                                    disabled={isSubmitted}
-                                ></button>
-                                <div className="ml-2">Down</div>
-                            </div>
-                        </div>
-                    </div>
-
-                    {!isSubmitted && (
-                        <div className="self-end ml-2">
-                            <button
-                                className={`text-white m-1 p-1 rounded-md ${
-                                    isSubmitted ? "disabled" : "bg-blue-500"
-                                }`}
-                                onClick={handleSubmit}
-                                disabled={isSubmitted}
-                            >
-                                Submit
-                            </button>
-                        </div>
-                    )}
-
-                    {isSubmitted && <div className="self-end ml-2"></div>}
-                </div>
-            </div>
-            <VoteNftModal
-                isConfirmationOpen={isConfirmationOpen}
-                handleConfirmation={handleConfirmation}
+  return (
+    <Box>
+      <Card className="lg:max-w-[800px] max-w-[460px] flex justify-center items-center mb-4 bg-gray-200 pb-12 pl-10 pr-10">
+        <Stack direction="row" spacing={4} className="flex">
+          <Box className="m-2">
+            <Text>Token Name</Text>
+            <Image
+              src="img/pieceNft.png"
+              alt="pieceNft"
+              className="w-20 h-20"
             />
-        </div>
-    );
+          </Box>
+          <Box className="m-2 p-1 text-center">
+            <Box className="text-blue-500">Offer Price</Box>
+            <Box className="text-xl font-bold">1.2 ETH</Box>
+          </Box>
+          <Box className="m-2 p-1 text-center">
+            <Box className="text-blue-500">Duration</Box>
+            <Box className="text-xl font-bold">0618~0619</Box>
+          </Box>
+          <Box className="m-2 p-1 text-center">
+            <Box className="text-blue-500">Vote</Box>
+            <Box>
+              <Box className="flex items-center mt-2">
+                <Button
+                  className={`option-button ${
+                    selectedOption === "up" ? "selected" : ""
+                  }`}
+                  onClick={() => handleOptionClick("up")}
+                  disabled={isSubmitted}
+                ></Button>
+                <Box className="ml-2 text-sm font-bold">Up</Box>
+              </Box>
+              <Box className="flex items-center mt-2">
+                <Button
+                  className={`option-button ${
+                    selectedOption === "down" ? "selected" : ""
+                  }`}
+                  onClick={() => handleOptionClick("down")}
+                  disabled={isSubmitted}
+                ></Button>
+                <Box className="ml-2 text-sm font-bold">Down</Box>
+              </Box>
+            </Box>
+          </Box>
+        </Stack>
+        {!isSubmitted && (
+          <Box className="self-end mt-2">
+            <Button
+              className={`text-white m-1 p-1 rounded-md ${
+                isSubmitted ? "disabled" : "bg-blue-500"
+              }`}
+              onClick={handleSubmit}
+              disabled={isSubmitted}
+            >
+              Submit
+            </Button>
+          </Box>
+        )}
+        {isSubmitted && <Box className="self-end ml-2"></Box>}
+        <Modal
+          isOpen={isConfirmationOpen}
+          onClose={() => handleConfirmation(false)}
+        >
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Confirmation</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>Are you sure you want to submit your vote?</ModalBody>
+            <ModalFooter>
+              <Button
+                colorScheme="green"
+                onClick={() => handleConfirmation(true)}
+              >
+                Confirm
+              </Button>
+              <Button
+                colorScheme="red"
+                ml={3}
+                onClick={() => handleConfirmation(false)}
+              >
+                Cancel
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      </Card>
+    </Box>
+  );
 };
 
 export default VoteNftCard;
