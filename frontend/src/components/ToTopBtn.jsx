55203469b5e3979f2bab909568c87c5f2496ee98
ljhmd00 <lmsmd00@naver.com>
2023-07-12 21:41:50 +0900

    수정

--- frontend/src/components/ToTopBtn.jsx
+++ frontend/src/components/ToTopBtn.jsx
@@ -2,44 +2,43 @@ import { useEffect, useState } from "react";
 import { Box } from "@chakra-ui/react";
 
 const TopBtn = () => {
-  const [showButton, setShowButton] = useState(false);
+    const [showButton, setShowButton] = useState(false);
 
-  const scrollToTop = () => {
-    window.scroll({
-      top: 0,
-      behavior: "smooth",
-    });
-  };
-  useEffect(() => {
-    const handleShowButton = () => {
-      if (window.scrollY > 500) {
-        setShowButton(true);
-      } else {
-        setShowButton(false);
-      }
+    const scrollToTop = () => {
+        window.scroll({
+            top: 0,
+            behavior: "smooth",
+        });
     };
+    useEffect(() => {
+        const handleShowButton = () => {
+            if (window.scrollY > 500) {
+                setShowButton(true);
+            } else {
+                setShowButton(false);
+            }
+        };
 
-    console.log(window.scrollY);
-    window.addEventListener("scroll", handleShowButton);
-    return () => {
-      window.removeEventListener("scroll", handleShowButton);
-    };
-  }, []);
+        window.addEventListener("scroll", handleShowButton);
+        return () => {
+            window.removeEventListener("scroll", handleShowButton);
+        };
+    }, []);
 
-  return (
-    showButton && (
-      <Box className="fixed bottom-20 right-5">
-        <Box
-          id="top"
-          onClick={scrollToTop}
-          type="button"
-          className=" rounded-full bg-black py-3 px-3 text-white font-[Tenada] cursor-pointer hover:bg-gray-600 border border-white"
-        >
-          {" "}
-          Top
-        </Box>
-      </Box>
-    )
-  );
+    return (
+        showButton && (
+            <Box className="fixed bottom-20 right-5">
+                <Box
+                    id="top"
+                    onClick={scrollToTop}
+                    type="button"
+                    className=" rounded-full bg-black py-3 px-3 text-white font-[Tenada] cursor-pointer hover:bg-gray-600 border border-white"
+                >
+                    {" "}
+                    Top
+                </Box>
+            </Box>
+        )
+    );
 };
 export default TopBtn;
