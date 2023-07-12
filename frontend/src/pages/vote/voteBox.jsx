7ae85652f2419f6accbc47a5c69cd792d2e9e506
sungyoon <crossbone48@gmail.com>
2023-07-12 15:19:36 +0900

    박스디자인

--- frontend/src/pages/vote/voteBox.jsx
+++ frontend/src/pages/vote/voteBox.jsx
@@ -118,93 +118,124 @@ const VoteBox = ({ account }) => {
   // }, [dataURI]);
 
   return (
-    <div>
-      return<div></div>
-    
-      <div
-        className={`lg:max-w-[800px] max-w-[460px] flex justify-center items-center mb-4 ${
-          isSubmitted ? "bg-gray-400" : "bg-gray-200"
-        } pb-12 pl-6`}
-      >
-        <div className="m-2">
-          <img src="img/pieceNft.png" alt="pieceNft" className="w-10 h-10" />
-        </div>
-        <div className="m-2 p-1 text-center">
-          <div>Offer price</div>
-          <div className="text-blue-500">1.2ETH</div>
-        </div>
-        <div className="m-2 p-1 text-center">
-          <div>Duration</div>
-          <div>0618~0619</div>
-        </div>
-        <div className="mt-6 ml-4">
-          <div>Vote</div>
-          <div>
-            <div className="flex items-center">
-              <button
-                className={`option-button ${
-                  selectedOption === "up" ? "selected" : ""
-                }`}
-                onClick={() => handleOptionClick("up")}
-                disabled={isSubmitted}
-              ></button>
-              <div className="ml-2">Up</div>
-            </div>
-            <div className="flex items-center">
-              <button
-                className={`option-button ${
-                  selectedOption === "down" ? "selected" : ""
-                }`}
-                onClick={() => handleOptionClick("down")}
-                disabled={isSubmitted}
-              ></button>
-              <div className="ml-2">Down</div>
-            </div>
-          </div>
-        </div>
-        {!isSubmitted && (
-          <div className="self-end ml-2">
-            <button
-              className={`text-white m-1 p-1 rounded-md ${
-                isSubmitted ? "disabled" : "bg-blue-500"
-              }`}
-              onClick={handleSubmit}
-              disabled={isSubmitted}
-            >
-              Submit
-            </button>
-          </div>
-        )}
-        {isSubmitted && <div className="self-end ml-2"></div>}
+    <Box>
+      {/* {dataURI ? (
+        dataURI > 0 ? ( */}
+      <Box className=" bg-gray-500 lg:max-w-[800px] max-w-[460px] border rounded-md p-2">
+        <div>
+          {dataURI?.map((v, i) => {
+            return (
+              <div>
+                <div
+                  key={i}
+                  className={`lg:max-w-[800px] max-w-[460px] flex justify-center items-center mb-4 ${
+                    isSubmitted ? "bg-gray-400" : "bg-gray-200"
+                  } pb-12 pl-6`}
+                >
+                  <div className="m-2">
+                    <img src={v.data.image} alt="pieceNft" className="w-10 h-10"/>
+                  </div>
+                  <div className="m-2 p-1 text-center">
+                    <div>Offer price</div>
+                    <div className="text-blue-500">{bestOffer} ETH</div>
+                  </div>
+                  <div className="m-2 p-1 text-center">
+                    <div className="">Duration</div>
+                    <div>0618~0619</div> // {/*기간도 설정해줘야함 */}
+                  </div>
+                  <div className="mt-6 ml-4">
+                    <div>Vote</div>
+                    <div>
+                      <div className="flex items-center">
+                        <button
+                          className={`option-button ${
+                            selectedOption === "up" ? "selected" : ""
+                          }`}
+                          onClick={() => handleOptionClick("up")}
+                          disabled={isSubmitted}
+                        ></button>
+                        <div className="ml-2">Up</div>
+                      </div>
+                      <div className="flex items-center">
+                        <button
+                          className={`option-button ${
+                            selectedOption === "down" ? "selected" : ""
+                          }`}
+                          onClick={() => handleOptionClick("down")}
+                          disabled={isSubmitted}
+                        ></button>
+                        <div className="ml-2">Down</div>
+                      </div>
+                    </div>
+                  </div>
+                  {!isSubmitted && (
+                    <div className="self-end ml-2">
+                      <button
+                        className={`text-white m-1 p-1 rounded-md ${
+                          isSubmitted ? "disabled" : "bg-blue-500"
+                        }`}
+                        onClick={handleSubmit}
+                        disabled={isSubmitted}
+                      >
+                        Submit
+                      </button>
+                    </div>
+                  )}
+                  {isSubmitted && <div className="self-end ml-2"></div>}
 
-        <Modal
-          isOpen={isConfirmationOpen}
-          onClose={() => handleConfirmation(false)}
-        >
-          <ModalOverlay />
-          <ModalContent>
-            <ModalHeader>Confirmation</ModalHeader>
-            <ModalCloseButton />
-            <ModalBody>Are you sure you want to submit your vote?</ModalBody>
-            <ModalFooter>
-              <Button
-                colorScheme="green"
-                onClick={() => handleConfirmation(true)}
-              >
-                Confirm
-              </Button>
-              <Button
-                colorScheme="red"
-                ml={3}
-                onClick={() => handleConfirmation(false)}
-              >
-                Cancel
-              </Button>
-            </ModalFooter>
-          </ModalContent>
-        </Modal>
-      </div>
-  </div>
+                  <Modal
+                    isOpen={isConfirmationOpen}
+                    onClose={() => handleConfirmation(false)}
+                  >
+                    <ModalOverlay />
+                    <ModalContent>
+                      <ModalHeader>Confirmation</ModalHeader>
+                      <ModalCloseButton />
+                      <ModalBody>
+                        Are you sure you want to submit your vote?
+                      </ModalBody>
+                      <ModalFooter>
+                        <Button
+                          colorScheme="green"
+                          onClick={() => handleConfirmation(true)}
+                        >
+                          Confirm
+                        </Button>
+                        <Button
+                          colorScheme="red"
+                          ml={3}
+                          onClick={() => handleConfirmation(false)}
+                        >
+                          Cancel
+                        </Button>
+                      </ModalFooter>
+                    </ModalContent>
+                  </Modal>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </Box>
+      {/* ) : (
+      <Box className="flex flex-col justify-center items-center mt-60">
+        <LuVote size={100} className=" text-red-600" />
+        <Text className="font-[Tenada] text-lg">
+          현재 진행중인 <sapn className="text-xl text-red-500">투표</sapn>가
+          없습니다.
+        </Text>
+      </Box>
+      ) ) : (
+      <Button
+        isLoading
+        loadingText="Loading"
+        colorScheme="blue"
+        variant="outline"
+      >
+        Loading
+      </Button>
+      )} */}
+    </Box>
   );
 };
 
