1776aefa8fa20c04cd2cbc9c2d03a6655ae4fb53
Doit-Goal <swspolice@naver.com>
2023-07-13 05:06:30 +0900

    수우우우우정

--- frontend/src/lib/web3.config.js
+++ frontend/src/lib/web3.config.js
@@ -6,11 +6,11 @@ import Web3 from "web3";
 const web3 = new Web3(window.ethereum);
 
 export const OGNFTContractAddress =
-  "0x08c7ae9D7ce2CE485c16a0C86255191636c13762";
+  "0x3F206801963c396508CaF50D04EEe724d5fB2183";
 export const MarketContractAddress =
-  "0x1Fc39Be85CCbb07dc0dc1264C54019419811257F";
+  "0xCadd9175D5d9b3997619f8206dd847729AC6Faa3";
 export const PieceMarketContractAddress =
-  "0xBaF6edfe279F027Ef3a359c08F9245d1d5a6C194";
+  "0xd861f1914ECDb8fEFA2CadFaE9dE722171EbCEe7";
 
 export const OGNFTContract = new web3.eth.Contract(
   OGNftABI,
