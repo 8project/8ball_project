9c7cd9163a2c1fe9e0eeea5d041eec741c10eb30
ljhmd00 <lmsmd00@naver.com>
2023-07-13 13:56:36 +0900

    myPieceNft.jsx(ui 수정)

--- frontend/src/pages/myPage/myPieceNft/myPieceNft.jsx
+++ frontend/src/pages/myPage/myPieceNft/myPieceNft.jsx
@@ -1,101 +1,102 @@
 import { Box, Text, Button } from "@chakra-ui/react";
 import MyPieceNftCard from "./myPieceNftCard";
 import { useEffect, useState } from "react";
-import {
-  MarketContract,
-  PieceMarketContractAddress,
-} from "../../../lib/web3.config";
+import { MarketContract, PieceMarketContractAddress } from "../../../lib/web3.config";
 
 const MyPieceNft = ({ account }) => {
-  const [pieceTokenIds, setPieceTokenIds] = useState([]);
-  const [isApproved, setIsApproved] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
+    const [pieceTokenIds, setPieceTokenIds] = useState([]);
+    const [isApproved, setIsApproved] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
 
-  const getMyNftTokenIds_Piece = async () => {
-    try {
-      const response = await MarketContract.methods
-        .getMyNftTokenId_Piece(account)
-        .call();
+    const getMyNftTokenIds_Piece = async () => {
+        try {
+            const response = await MarketContract.methods.getMyNftTokenId_Piece(account).call();
 
-      const userPieceTokenIdArray = response?.map((v) => {
-        return Number(v);
-      });
-      setPieceTokenIds(userPieceTokenIdArray);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+            const userPieceTokenIdArray = response?.map((v) => {
+                return Number(v);
+            });
+            setPieceTokenIds(userPieceTokenIdArray);
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
-  const checkApprovalStatus = async () => {
-    try {
-      const checkApproved = await MarketContract.methods
-        .isApprovedForAll(account, PieceMarketContractAddress)
-        .call({ from: account });
-      setIsApproved(checkApproved);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+    const checkApprovalStatus = async () => {
+        try {
+            const checkApproved = await MarketContract.methods
+                .isApprovedForAll(account, PieceMarketContractAddress)
+                .call({ from: account });
+            setIsApproved(checkApproved);
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
-  const onClickPieceApprove = async (e) => {
-    e.preventDefault();
+    const onClickPieceApprove = async (e) => {
+        e.preventDefault();
 
-    try {
-      setIsLoading(true);
+        try {
+            setIsLoading(true);
 
-      const response = await MarketContract.methods
-        .setApprovalForAll(PieceMarketContractAddress, true)
-        .send({ from: account });
+            const response = await MarketContract.methods
+                .setApprovalForAll(PieceMarketContractAddress, true)
+                .send({ from: account });
 
-      if (response.status) {
-        setIsApproved(true);
-      }
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setIsLoading(false);
-    }
-  };
+            if (response.status) {
+                setIsApproved(true);
+            }
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setIsLoading(false);
+        }
+    };
 
-  useEffect(() => {
-    getMyNftTokenIds_Piece();
-    checkApprovalStatus();
-  }, []);
+    useEffect(() => {
+        getMyNftTokenIds_Piece();
+        checkApprovalStatus();
+    }, []);
 
-  return (
-    <Box className="mt-[82px] mb-[72px] lg:max-w-[800px] max-w-[460px]">
-      <Text>
-        My Piece NFT
-        <Box>
-          Sell Status{" "}
-          {isApproved ? (
-            <Button colorScheme="green" size="sm" variant="outline">
-              Possible
-            </Button>
-          ) : (
-            <Button
-              colorScheme="red"
-              size="sm"
-              variant="outline"
-              onClick={onClickPieceApprove}
-              disabled={isLoading}
-            >
-              {isLoading ? "Loading..." : "Approved"}
-            </Button>
-          )}
+    return (
+        <Box className="mt-[60px] mb-[72px] lg:max-w-[800px] max-w-[460px]">
+            <Text className="text-center mt-10 font-[Tenada]  lg:text-xl text-md">
+                My Piece NFT
+            </Text>
+            <Box className="mt-6 flex flex-col justify-center items-center">
+                <Box className="flex items-center border rounded-lg px-4 py-2">
+                    <Text className="font-[Tenada] mr-2">Sell Status</Text>
+                    {isApproved ? (
+                        <Button colorScheme="green" size="sm" variant="outline">
+                            Possible
+                        </Button>
+                    ) : (
+                        <Button
+                            colorScheme="red"
+                            size="sm"
+                            variant="outline"
+                            onClick={onClickPieceApprove}
+                            disabled={isLoading}
+                        >
+                            {isLoading ? "Loading..." : "Approved"}
+                        </Button>
+                    )}
+                </Box>
+            </Box>
+            <Box className="mt-4 grid lg:grid-cols-2 gap-14">
+                {pieceTokenIds.map((p, i) => (
+                    <Box>
+                        <MyPieceNftCard
+                            key={i}
+                            pieceId={p}
+                            account={account}
+                            isApproved={isApproved}
+                            onClickPieceApprove={onClickPieceApprove}
+                        />
+                    </Box>
+                ))}
+            </Box>
         </Box>
-      </Text>
-      {pieceTokenIds.map((p, i) => (
-        <MyPieceNftCard
-          key={i}
-          pieceId={p}
-          account={account}
-          isApproved={isApproved}
-          onClickPieceApprove={onClickPieceApprove}
-        />
-      ))}
-    </Box>
-  );
+    );
 };
 
 export default MyPieceNft;
